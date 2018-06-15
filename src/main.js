9c36c2fac907c7f0d7b38d81a741c7bfba5ffbb4
speake <samp97@vt.edu>
2018-06-15 15:47:47 -0400

    Added movie-list and movie-filter components

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import './style.scss';
+import Vue from 'vue';
+import genres from './util/genres.js';
+
+
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list': {
+      template: `
+        <div id="movie-list">
+          <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+        </div>
+      `,
+      data: function() {
+        return {
+          movies: [
+            { title: 'Pulp Fiction'},
+            { title: 'Home Alone'},
+            { title: 'Austin Powers'}
+          ]
+        };
+      }
+    },
+    'movie-filter': {
+      data() {
+        return {
+          genres
+        };
+      },
+      template: `
+        <div id="movie-filter">
+          <h2>Filter results</h2>
+          <div class="filter-group">
+            <check-filter v-for="genre in genres"></check-filter>
+          </div>
+        </div>
+      `,
+      components: {
+        'check-filter': {
+          template: `
+          <div>Filter</div>
+          `
+        }
+      }
+    }
+  }
+});
