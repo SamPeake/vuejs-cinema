fc0f5375741828cde45619de2337059b2c0235c7
speake <samp97@vt.edu>
2018-06-18 17:36:37 -0400

    Added time filters. Has bugs

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="movie-filter">
     <h2>Filter results</h2>
+    <h3>By time of day</h3>
     <div class="filter-group">
-      <check-filter v-for="genre in genres" :title="genre" v-on:check-filter="checkFilter"></check-filter>
+      <check-filter v-for="time in times" category="time" :title="time" v-on:check-filter="checkFilter"></check-filter>
+    </div>
+    <h3>By genre</h3>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres" category="genre" :title="genre" v-on:check-filter="checkFilter"></check-filter>
     </div>
   </div>
 </template>
 
 <script>
 
-  import genres from '../util/genres.js';
+  import genres from '../util/genres';
+  import times from '../util/times';
   import CheckFilter from './CheckFilter.vue';
 
   export default {
 
       data() {
         return {
-          genres
-        }
+          genres,
+          times
+        };
       },
 
       methods: {
