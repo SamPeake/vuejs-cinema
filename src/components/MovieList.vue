c2cb842547aa135fd2d8eae61316928d9eec95a6
speake <samp97@vt.edu>
2018-06-18 14:50:16 -0400

    rearranged Vue components into separate single  file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="movie-list">
+      <div v-for="movie in filteredMovies" class="movie">
+      {{   movie.title }}
+      </div>
+    </div>
+</template>
+<script>
+    import genres from '../util/genres.js';
+
+
+    export default {
+
+        data: function() {
+          return {
+            movies: [
+              { title: 'Pulp Fiction', genre: genres.CRIME },
+              { title: 'Home Alone', genre: genres.COMEDY },
+              { title: 'Austin Powers', genre: genres.COMEDY }
+            ]
+          };
+        },
+        props: [
+          'genre',
+          'time'
+        ],
+        methods: {
+          moviePassesGenreFilter: function(movie) {
+            if (!this.genre.length) {
+              return true;
+            } else {
+              return this.genre.find(genre => movie.genre === genre);
+            }
+          }
+        },
+        computed: {
+          filteredMovies: function() {
+            return this.movies.filter(this.moviePassesGenreFilter);
+          }
+        }
+      }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './style.scss';
 import Vue from 'vue';
 import genres from './util/genres.js';
-
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
   el: '#app',
@@ -22,83 +23,7 @@ new Vue({
     }
   },
   components: {
-    'movie-list': {
-      template: `
-        <div id="movie-list">
-          <div v-for="movie in filteredMovies" class="movie">
-          {{   movie.title }}
-          </div>
-        </div>
-      `,
-      data: function() {
-        return {
-          movies: [
-            { title: 'Pulp Fiction', genre: genres.CRIME },
-            { title: 'Home Alone', genre: genres.COMEDY },
-            { title: 'Austin Powers', genre: genres.COMEDY }
-          ]
-        };
-      },
-      props: [
-        'genre',
-        'time'
-      ],
-      methods: {
-        moviePassesGenreFilter: function(movie) {
-          if (!this.genre.length) {
-            return true;
-          } else {
-            return this.genre.find(genre => movie.genre === genre);
-          }
-        }
-      },
-      computed: {
-        filteredMovies: function() {
-          return this.movies.filter(this.moviePassesGenreFilter);
-        }
-      }
-    },
-    'movie-filter': {
-      data() {
-        return {
-          genres
-        }
-      },
-      template: `
-        <div id="movie-filter">
-          <h2>Filter results</h2>
-          <div class="filter-group">
-            <check-filter v-for="genre in genres" :title="genre" v-on:check-filter="checkFilter"></check-filter>
-          </div>
-        </div>
-      `,
-      methods: {
-        checkFilter: function(category, title, checked) {
-          this.$emit('check-filter', category, title, checked);
-        }
-      },
-      components: {
-        'check-filter': {
-          data(){
-            return {
-              checked: false
-            }
-          },
-          props: [ 'title' ],
-          template: `
-          <div :class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
-            <span class='checkbox'></span>
-            <span class='check-filter-title'>{{ title }}</span>
-          </div>
-          `,
-          methods: {
-            checkFilter() {
-              this.checked = !this.checked;
-              this.$emit('check-filter', 'genre', this.title, this.checked);
-            }
-          }
-        }
-      }
-    }
+    MovieList,
+    MovieFilter
   }
 });
