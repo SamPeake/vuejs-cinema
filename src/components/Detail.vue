cf04a4b19722fc5241b1dfee0cb634830449df90
speake <samp97@vt.edu>
2018-06-19 12:02:01 -0400

    Added router links in video titles.

---
+++ src/components/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="detail" v-if="movie">
+      <movie-item :movie="movie.movie" :sessions="[]"></movie-item>
+    </div>
+</template>
+<script>
+    import MovieItem from './MovieItem.vue';
+
+    export default {
+      props: [ 'movies'],
+      computed: {
+          movie: function() {
+            let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+            return movie ? movie : null;
+          }
+      },
+      created: function() {
+            console.log(this.$route.params.id);
+      },
+      components: {
+        MovieItem
+      }
+    }
+</script>
